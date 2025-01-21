0ed0d33e6b2806afe6def43925ad001ce128298b
Nilambar Sharma <nilambar@outlook.com>
2025-01-21 17:23:56 +0545

    Color scheme

--- widgetizer.php
+++ widgetizer.php
@@ -17,7 +17,6 @@ if ( ! defined( 'WIDGETIZER_URL' ) ) {
 	define( 'WIDGETIZER_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 }
 
-
 if ( ! function_exists( 'widgetizer_init' ) ) {
 
 	/**
@@ -29,8 +28,28 @@ if ( ! function_exists( 'widgetizer_init' ) ) {
 		add_action(
 			'admin_enqueue_scripts',
 			function () {
+
 				wp_enqueue_style( 'widgetizer-style', WIDGETIZER_URL . '/assets/widgetizer.css', [], WIDGETIZER_VERSION );
 				wp_enqueue_script( 'widgetizer-script', WIDGETIZER_URL . '/assets/widgetizer.js', [], WIDGETIZER_VERSION, true );
+
+				$css_variables = apply_filters(
+					'widgetizer_css_variables',
+					[
+						'color-primary' => '#2271b1',
+					]
+				);
+
+				if ( ! empty( $css_variables ) ) {
+					$styles = '';
+
+					foreach ( $css_variables as $key => $value ) {
+						$styles .= '--widgetizer-' . esc_attr( $key ) . ':' . esc_attr( $value ) . ';';
+					}
+
+					$custom_css = ':root { ' . $styles . ' }';
+
+					wp_add_inline_style( 'common', $custom_css );
+				}
 			}
 		);
 	}
